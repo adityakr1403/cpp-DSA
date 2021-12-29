624989dae3d3c1ea60bdf6c079db7008773dd27d
adityakr1403 <adityakr1403@gmail.com>
2021-12-29 19:52:00 +0530

    Update knapsack_Rec.cpp

--- knapsack_Rec.cpp
+++ knapsack_Rec.cpp
@@ -10,11 +10,11 @@ int knapsack(int wt[], int val[], int W, int n)
     }
     if (wt[n - 1] <= W)
     {
-        return max(knapsack(wt, val, W - wt[n - 1], n - 1), knapsack(wt, val, W, n - 1));
+        return max(val[n - 1] + knapsack(wt, val, W - wt[n - 1], n - 1), knapsack(wt, val, W, n - 1));
     }
     else // if (wt[n - 1] > W)
     {
-        return 0;
+        return knapsack(wt, val, W, n - 1);
     }
 }
 
@@ -23,5 +23,7 @@ int main()
     int wt[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int val[] = {1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
     int W = 4;
-    cout << knapsack(wt, val, W, 10) << endl;
+    cout << "\n"
+         << knapsack(wt, val, W, 10) << "\n"
+         << endl;
 }

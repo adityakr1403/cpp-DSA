bd2fed6df00519c7dea4e4590308030226e25a61
adityakr1403 <adityakr1403@gmail.com>
2021-12-29 19:02:24 +0530

    Update knapsack_Rec.cpp

--- knapsack_Rec.cpp
+++ knapsack_Rec.cpp
@@ -13,6 +13,10 @@ int knapsack(int wt[], int val[], int W, int n)
     {
         return max(knapsack(wt, val, W - wt[n - 1], n - 1), knapsack(wt, val, W, n - 1));
     }
+    else if (wt[n - 1] > W)
+    {
+        return 0;
+    }
 }
 
 int main()
